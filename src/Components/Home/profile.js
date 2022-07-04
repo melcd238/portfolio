70c3ae8d9dea65dcf7c0a3b49f15b155a96644b4
melcd238 <melcd238@gmail.com>
2022-07-04 18:20:35 +0200

    container home

---
+++ src/Components/Home/profile.js
@@ -0,0 +1,46 @@
+import TypeAnimation from 'react-type-animation';
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import melPicture from '../../Assets/melProfil.jpg';
+import "./profile.css"
+
+const Profil = ()=>{
+    return(
+        <div className="profil_container">
+            <div className="profil_resume">
+                <h1>👋 Je suis Mélaïna Donati 👋 </h1>
+               <div className='profil_animation'>
+                <TypeAnimation
+                     cursor={false}
+                     sequence={["Développeuse d'application React.js", 2000, "Développeuse MERN 💻  ", 2000, "Enthousiaste et ultra motivée 😄",2000]}
+                     wrapper="h2"
+                     repeat={Infinity}/>
+                </div>
+
+                <p>Amoureuse de React.js et de son ecosystème, j'ai une appétence pour le frontend...</p>
+                <p>J'apprécie aussi construire des applications MERN</p> 
+
+                <div className='profil_btn'>
+                    <div><button className='btn'>Me contacter</button></div>
+                    <div>
+                    <button className='btn'><a href="mdonatiCV.pdf" download="mdonatiCV.pdf">Mon CV</a></button>
+                    </div>
+                </div> 
+
+                <div className='profil_social_media'>
+                    <a href="https://www.linkedin.com/in/mel-donati-1aa4081b0/" target="_blank" rel="noreferrer"><FaLinkedinIn style={{color:"white", fontSize:"30px"}}/></a>
+                    <a href="https://github.com/melcd238" target="_blank" rel="noreferrer"><FaGithub style={{color:"white", fontSize:"30px"}}/></a>
+                </div>   
+
+            </div>
+            <div className="profil_picture">
+                <div className='profil_picture_border'>
+                   <img src={melPicture} alt="profil de mel donati"></img>
+                </div>
+
+            </div>
+        
+        </div>
+    )
+}
+
+export default Profil
---
+++ src/PortfolioContainer/Home/Home.js
@@ -0,0 +1,12 @@
+import Profil from "../../Components/Home/profile"
+import './Home.css'
+
+const Home = ()=>{
+return(
+    <div className="home_container">
+        <Profil/>
+    </div>
+)
+}
+
+export default Home
