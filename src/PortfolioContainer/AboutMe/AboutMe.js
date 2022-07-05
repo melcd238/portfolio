798c7b3709486e3563a37afc62615adacfbe7610
melcd238 <melcd238@gmail.com>
2022-07-05 19:05:20 +0200

    Component AboutMe starting

---
+++ src/PortfolioContainer/AboutMe/AboutMe.js
@@ -0,0 +1,17 @@
+import "./AboutMe.css";
+import ScreenTitle from '../../Components/ScreenTitle/ScreenTitle'
+
+
+
+const AboutMe= ()=>{
+    return(
+        <div>
+            <div className="about-me-parent">
+                <ScreenTitle title="About Me" subtitle="Pourquoi me choisir?"/>
+            </div>
+      
+        </div>
+    )
+}
+
+export default AboutMe
