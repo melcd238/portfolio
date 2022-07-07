30202c978aade117d7960188f73a3cbe76fa410e
melcd238 <melcd238@gmail.com>
2022-07-07 19:18:53 +0200

    fix bug css resume responsive

--- src/Components/MyWorks/Card.js
+++ src/Components/MyWorks/Card.js
@@ -3,7 +3,8 @@ import { motion } from 'framer-motion'
 
 const Card = ()=>{
     return(
-        <motion.div className='card-container'>
+        <motion.div className='card-container' whileHover={{ scale: 1.04 }}
+        whileTap={{ scale: 0.96 }}>
         card
         </motion.div>
     )
