c4272d3755ca1e3675124a9e294a8dd2ad9aac87
melcd238 <melcd238@gmail.com>
2022-08-13 16:41:05 +0200

    udpate about me

--- src/PortfolioContainer/AboutMe/AboutMe.js
+++ src/PortfolioContainer/AboutMe/AboutMe.js
@@ -34,17 +34,18 @@ const AboutMe= ({id})=>{
                    </div>
                    <div className="about-me-details">
                    <div className="about-me-trait"></div>
-                        <p>Après une carrière comme chirurgien dentiste et des problèmes de santé m'empêchant d'exercer, c'est tout naturellement que je me suis tournée vers
-                            une reconversion dans le développement web. Je souhaite continuer à rendre service et faire en sorte que mes mains soient le prolongement de mon cerveau
-                            mais d'une manière différente. J'ai ainsi validé une première formation de développeuse web chez OpenClassrooms. Ayant à coeur de sans cesse progresser et apprendre,
-                            j'ai pousuivi ma formation de développeuse d'application Javascript - React.js.<br/>
+                        <p>Après une carrière comme chirurgien dentiste, souhaitant continuer à rendre service et à utiliser mes mains comme
+                            le prolongement de mon cerveau, c'est tout naturellement que je me suis tournée vers
+                            une reconversion dans le développement web. 
+                            J'ai ainsi validé une première formation de développeuse web chez OpenClassrooms. Ayant à coeur de sans cesse progresser et apprendre,
+                            j'ai pousuivi une formation de développeuse d'application Javascript - React.js.<br/>
                             Trouver des solutions aux problèmes posés a toujours fait partie de ma vie, que ce soit en tant 
                              que dentiste ou en tant que développeuse. C'est le moteur de mon épanouissement:
                             .... et quelle sensation géniale que de ressentir la montée d'adrénaline, de joie et de fierté à l'aboutissement d'un projet.
-                            Ayant une appétence pour le Frontend , je suis  passionnée  par React.js et son écosystème mais le backend ne reste jamais loin non plus.
+                            Ayant une appétence pour le Frontend , je suis  passionnée  par React.js et son écosystème mais le backend (Node.js) n'est jamais loin non plus.
                             Les méandres de la vie m'ont appris à croire en moi:  même dans les situations qui semblent perdues, je reste positive.<br/> 
                             Aguerrie à la communication, je sais travailler en équipe, être force de propositions et n'attend plus qu'une équipe pour continuer à apprendre auprès de mon (mes) Yoda(s).<br/>
-                            La vie professionnelle n'étant qu'une partie d'un tout, je puise mon énergie au sein de ma famille, de la musique, et mon oxygène en montagne, dès que je le peux,
+                            La vie professionnelle n'étant qu'une partie d'un tout, je puise mon énergie au sein de ma famille, de la musique et mon oxygène en montagne, dès que je le peux,
                             été comme hiver.<br/>
                             Si mon profil attire votre curiosité, n'hésitez pas à me contacter...
                         </p>
